09284fa0183b0d9a117f6e824b84a3c2ebb236a4
jean <leejeanhwa@gmail.com>
2022-04-30 19:32:24 +0800

    fix script and update cypress v9.6.0

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,18 +1,21 @@
 /// <reference types='cypress' />
 
 it('Test Carousel Slider', () => {
-    const data = 'Nicole Kong'
+    const data = 'Wong Kee Vin'
     cy.visit('/#about')
     cy.get('.team-carousel').should('be.visible').scrollIntoView()
-    function scrollCarousel(data) {
-        cy.contains('.team-carousel .owl-item', data).then(e => {
-            if (Cypress.dom.isVisible(e))
-                cy.log('Found!')
-            else {
-                cy.get('.team-carousel .owl-next').click()
-                scrollCarousel(data)
-            }
-        })
-    }
+    cy.get('div.team-carousel div.owl-item').as('lstItems')
+    .should('have.length.greaterThan', 0)
     scrollCarousel(data)
 })
+
+function scrollCarousel(data) {
+    cy.get('@lstItems').contains(data)
+    .then(e => {
+        if (!Cypress.dom.isVisible(e)) {
+            cy.get('.team-carousel .owl-next').click()
+            return scrollCarousel(data)
+        }
+        cy.log('Found!')
+    })
+}
